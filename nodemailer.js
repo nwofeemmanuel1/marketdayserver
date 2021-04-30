db0f9d8d411d0beadff5e1f74b0c1f10c75b16f5
Nwofe Emmanuel <enwofe042@gmail.com>
2021-04-30 06:13:45 -0700

    changed nodemailer'

--- nodemailer.js
+++ nodemailer.js
@@ -1,43 +1,43 @@
-// const config=require('config')
-// const nodemailer=require('nodemailer');
-// // console.log(config.get('mailoptions.email'))
-// // const SMTPTransport = require('nodemailer/lib/smtp-transport');
-// // const smtpTransport = require('nodemailer-smtp-transport');
-// const sendemail=(recieverEmail,subject,text)=>{
-
-// const transporter=nodemailer.createTransport({
-//     service:"gmail",
-//     // host:"smtp.gmail.com",
-//     auth:{
-//         user:config.get('mailoptions.email'),
-//         pass:config.get('mailoptions.pasword')
-//     }
-// })
-// const mailOptions={
-//     from:config.get('mailoptions.email'),
-//     to:recieverEmail,
-//     subject:subject,
-//     text:text,
-//     attachments:[{
-//     filename:"picture.jpg",
-//     path:'./picture.jpg'
-// }]
-// };
-
-// transporter.sendMail(mailOptions,function(error,info){
-//     if(error){
-//         console.log(error.message)
-//     }else{
-//         console.log("email sent successfully with staus code : " +info.response)
-//     }
-// })
-
-
-// }
-
-// sendemail('enwofe2021@gmail.com','Wow Am happy to use nodemailer and send file','well i have actually made the nodemailer email support working on my computer and am happy to send u email with it')
-
-
-
 const config=require('config')
-console.log(config.get('connection.url'))
+const nodemailer=require('nodemailer');
+// console.log(config.get('mailoptions.email'))
+// const SMTPTransport = require('nodemailer/lib/smtp-transport');
+// const smtpTransport = require('nodemailer-smtp-transport');
+const sendemail=(recieverEmail,subject,text)=>{
+
+const transporter=nodemailer.createTransport({
+    service:"gmail",
+    // host:"smtp.gmail.com",
+    auth:{
+        user:config.get('mailoptions.email'),
+        pass:config.get('mailoptions.pasword')
+    }
+})
+const mailOptions={
+    from:config.get('mailoptions.email'),
+    to:recieverEmail,
+    subject:subject,
+    text:text,
+    attachments:[{
+    filename:"picture.jpg",
+    path:'./picture.jpg'
+}]
+};
+
+transporter.sendMail(mailOptions,function(error,info){
+    if(error){
+        console.log(error.message)
+    }else{
+        console.log("email sent successfully with staus code : " +info.response)
+    }
+})
+
+
+}
+
+sendemail('enwofe2021@gmail.com','Wow Am happy to use nodemailer and send file','well i have actually made the nodemailer email support working on my computer and am happy to send u email with it')
+
+
+
+// const config=require('config')
+// console.log(config.get('connection.url'))
